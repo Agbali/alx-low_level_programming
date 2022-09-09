d78d8351c2da29ade2a52a0319942f7c234328a7
Blessing Agbali <blessagbali20@gmail.com>
2022-09-09 15:20:00 -0700

    removed trailing whitespaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,10 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ *main - entry point
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
@@ -11,18 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0) 
-	{
-		/* if condition is true then print the following */
-		printf("%d is positive\n", n);	
-	}
-	else if (n == 0) 
-	{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	}
-	else 
-	{
+	else
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
